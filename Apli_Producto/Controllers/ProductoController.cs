00e133a7cf0a41ccc5187109cb728cab83dbb651
Kenel Cruz <kenelcruz@gmail.com>
2021-05-16 22:15:15 -0400

    Agregar archivos de proyecto.

---
+++ Apli_Producto/Controllers/ProductoController.cs
@@ -0,0 +1,90 @@
+﻿
+using Apli_Producto.Context;
+using Apli_Producto.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Apli_Producto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public ProductoController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return context.Producto.ToList();
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(string id)
+        {
+            var producto = context.Producto.FirstOrDefault(p => p.Codigo == id);
+            return producto;
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Producto producto)
+        {
+            try
+            {
+            context.Producto.Add(producto);
+            context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+           
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Producto producto)
+        {
+            if (producto.Codigo == id)
+            {
+                context.Entry(producto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var producto = context.Producto.FirstOrDefault(predicate => predicate.Codigo == id);
+            if (producto!=null)
+            {
+                context.Producto.Remove(producto);
+                context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(); 
+            }
+        }
+    }
+}
---
+++ Apli_Producto/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apli_Producto.Models
+{
+    public class Producto
+    {
+        [Key]
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Costo { get; set; }
+    }
+}
